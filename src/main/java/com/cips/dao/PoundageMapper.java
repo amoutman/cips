8421700a146dabd1a41c32515f9ebd4fa53d2dd8
Simon <Simon@Simon-PC>
2016-01-13 18:39:27 +0800

    yushusheng

---
+++ src/main/java/com/cips/dao/PoundageMapper.java
@@ -0,0 +1,17 @@
+package com.cips.dao;
+
+import com.cips.model.Poundage;
+
+public interface PoundageMapper {
+    int deleteByPrimaryKey(String id);
+
+    int insert(Poundage record);
+
+    int insertSelective(Poundage record);
+
+    Poundage selectByPrimaryKey(String id);
+
+    int updateByPrimaryKeySelective(Poundage record);
+
+    int updateByPrimaryKey(Poundage record);
+}
---
+++ src/main/java/com/cips/model/Poundage.java
@@ -0,0 +1,76 @@
+package com.cips.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Poundage {
+    private String id;
+
+    private BigDecimal poundageAmount;
+
+    private Integer status;
+
+    private String createdId;
+
+    private Date createdDate;
+
+    private String modifiedId;
+
+    private Date modifiedDate;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public BigDecimal getPoundageAmount() {
+        return poundageAmount;
+    }
+
+    public void setPoundageAmount(BigDecimal poundageAmount) {
+        this.poundageAmount = poundageAmount;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getCreatedId() {
+        return createdId;
+    }
+
+    public void setCreatedId(String createdId) {
+        this.createdId = createdId == null ? null : createdId.trim();
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getModifiedId() {
+        return modifiedId;
+    }
+
+    public void setModifiedId(String modifiedId) {
+        this.modifiedId = modifiedId == null ? null : modifiedId.trim();
+    }
+
+    public Date getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(Date modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+}
